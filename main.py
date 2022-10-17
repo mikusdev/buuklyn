ed0e775ffa97f55aba335b3a32858356b82f2f9f
Mikus <92790899+mikusdev@users.noreply.github.com>
2022-10-17 18:49:38 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify, render_template
+from numpy import DataSource
+from scripts import email_client
+import time
+
+app = Flask("app")
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/booking', methods=['GET'])
+def booking():
+    email_client.email("switch.mc.astro@gmail.com","test")
+
+@app.route('/api', methods=['POST'])
+def api():
+    return "api called with data: " + request.form['data'] + " error: decaprated/removed/not implemented"
+
+@app.route('/api/booking', methods=['POST'])
+def api_booking():
+    return "api called with data: " + request.form['data'] + " error: decaprated/removed/not implemented"
+
+app.run("0.0.0.0", debug=True)
